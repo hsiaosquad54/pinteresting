bde93b15a5f47957afd0e8d634f9fffc36397bc8
Steven Hsiao <shsiao@gmail.com>
2016-06-05 08:26:27 -0700

    Added jQuery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,9 @@
 //= require jquery
 //= require bootstrap
 //= require jquery_ujs
+
+//= require jquery.turbolinks
+//= require turbolinks
+
 //= require masonry/jquery.masonry
 //= require_tree .
